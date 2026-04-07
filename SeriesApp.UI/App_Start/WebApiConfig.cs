472cade49ae868f1959b1b021b9b4770eedaad6d
bipinnegi <bipinnegi.220112935@gehu.ac.in>
2026-04-07 22:04:14 +0530

    Registered Web API in Global.asax

--- SeriesApp.UI/App_Start/WebApiConfig.cs
+++ SeriesApp.UI/App_Start/WebApiConfig.cs
@@ -1,11 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Web.Http;
 
-namespace SeriesApp.UI.App_Start
+namespace SeriesApp.UI
 {
-    public class WebApiConfig
+    public static class WebApiConfig
     {
+        public static void Register(HttpConfiguration config)
+        {
+            // Enable attribute routing
+            config.MapHttpAttributeRoutes();
+
+            // Define default API route
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{action}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
     }
 }
