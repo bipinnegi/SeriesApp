4ff2cd65afb6cb6b86f238f7f36201b9fb1e99b9
bipinnegi <bipinnegi.220112935@gehu.ac.in>
2026-04-07 13:28:47 +0530

    Step 3: Implemented DAL (DBHelper + SeriesRepository with stored procedure calls)

---
+++ SeriesApp.DAL/DBHelper/DBHelper.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace SeriesApp.DAL.DBHelper
+{
+    public class DBHelper
+    {
+        private readonly string connectionString;
+
+        public DBHelper()
+        {
+            connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+        }
+
+        public SqlConnection GetConnection()
+        {
+            return new SqlConnection(connectionString);
+        }
+
+        public DataTable ExecuteSelectCommand(string commandText, CommandType commandType, SqlParameter[] parameters = null)
+        {
+            using (SqlConnection conn = GetConnection())
+            {
+                using (SqlCommand cmd = new SqlCommand(commandText, conn))
+                {
+                    cmd.CommandType = commandType;
+
+                    if (parameters != null)
+                        cmd.Parameters.AddRange(parameters);
+
+                    using (SqlDataAdapter da = new SqlDataAdapter(cmd))
+                    {
+                        DataTable dt = new DataTable();
+                        da.Fill(dt);
+                        return dt;
+                    }
+                }
+            }
+        }
+
+        public int ExecuteNonQuery(string commandText, CommandType commandType, SqlParameter[] parameters = null)
+        {
+            using (SqlConnection conn = GetConnection())
+            {
+                using (SqlCommand cmd = new SqlCommand(commandText, conn))
+                {
+                    cmd.CommandType = commandType;
+
+                    if (parameters != null)
+                        cmd.Parameters.AddRange(parameters);
+
+                    conn.Open();
+                    return cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public object ExecuteScalar(string commandText, CommandType commandType, SqlParameter[] parameters = null)
+        {
+            using (SqlConnection conn = GetConnection())
+            {
+                using (SqlCommand cmd = new SqlCommand(commandText, conn))
+                {
+                    cmd.CommandType = commandType;
+
+                    if (parameters != null)
+                        cmd.Parameters.AddRange(parameters);
+
+                    conn.Open();
+                    return cmd.ExecuteScalar();
+                }
+            }
+        }
+    }
+}
---
+++ SeriesApp.DAL/Repository/SeriesRepository.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using SeriesApp.DAL.DBHelper;
+
+namespace SeriesApp.DAL.Repository
+{
+    public class SeriesRepository
+    {
+        private readonly DBHelper.DBHelper dbHelper;
+
+        public SeriesRepository()
+        {
+            dbHelper = new DBHelper.DBHelper();
+        }
+
+        public int InsertSeries(string title, string description, int releaseYear, string genre)
+        {
+            SqlParameter[] parameters = new SqlParameter[]
+            {
+                new SqlParameter("@Title", title),
+                new SqlParameter("@Description", description),
+                new SqlParameter("@ReleaseYear", releaseYear),
+                new SqlParameter("@Genre", genre)
+            };
+
+            object result = dbHelper.ExecuteScalar("sp_InsertSeries", CommandType.StoredProcedure, parameters);
+
+            return Convert.ToInt32(result);
+        }
+
+        public int UpdateSeries(int seriesId, string title, string description, int releaseYear, string genre)
+        {
+            SqlParameter[] parameters = new SqlParameter[]
+            {
+                new SqlParameter("@SeriesId", seriesId),
+                new SqlParameter("@Title", title),
+                new SqlParameter("@Description", description),
+                new SqlParameter("@ReleaseYear", releaseYear),
+                new SqlParameter("@Genre", genre)
+            };
+
+            return dbHelper.ExecuteNonQuery("sp_UpdateSeries", CommandType.StoredProcedure, parameters);
+        }
+
+        public DataTable SearchSeries(string title, int? releaseYear)
+        {
+            SqlParameter[] parameters = new SqlParameter[]
+            {
+                new SqlParameter("@Title", (object)title ?? DBNull.Value),
+                new SqlParameter("@ReleaseYear", (object)releaseYear ?? DBNull.Value)
+            };
+
+            return dbHelper.ExecuteSelectCommand("sp_SearchSeries", CommandType.StoredProcedure, parameters);
+        }
+    }
+}
