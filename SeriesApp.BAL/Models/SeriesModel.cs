b46f77fa01fcc3f056a97c0f60edd825eb4c2025
bipinnegi <bipinnegi.220112935@gehu.ac.in>
2026-04-07 13:43:39 +0530

    Implemented BAL (SeriesModel + SeriesService with validation and mapping)

---
+++ SeriesApp.BAL/Models/SeriesModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SeriesApp.BAL.Models
+{
+    public class SeriesModel
+    {
+        public int SeriesId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int ReleaseYear { get; set; }
+        public string Genre { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+    }
+}
---
+++ SeriesApp.BAL/Services/SeriesService.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using SeriesApp.DAL.Repository;
+using SeriesApp.BAL.Models;
+
+namespace SeriesApp.BAL.Services
+{
+    public class SeriesService
+    {
+        private readonly SeriesRepository repository;
+
+        public SeriesService()
+        {
+            repository = new SeriesRepository();
+        }
+
+        public int AddSeries(SeriesModel model)
+        {
+            ValidateModel(model);
+
+            return repository.InsertSeries(
+                model.Title,
+                model.Description,
+                model.ReleaseYear,
+                model.Genre
+            );
+        }
+
+        public int UpdateSeries(SeriesModel model)
+        {
+            ValidateModel(model);
+
+            return repository.UpdateSeries(
+                model.SeriesId,
+                model.Title,
+                model.Description,
+                model.ReleaseYear,
+                model.Genre
+            );
+        }
+
+        public List<SeriesModel> SearchSeries(string title, int? releaseYear)
+        {
+            DataTable dt = repository.SearchSeries(title, releaseYear);
+
+            List<SeriesModel> list = new List<SeriesModel>();
+
+            foreach (DataRow row in dt.Rows)
+            {
+                list.Add(new SeriesModel
+                {
+                    SeriesId = Convert.ToInt32(row["SeriesId"]),
+                    Title = row["Title"].ToString(),
+                    Description = row["Description"].ToString(),
+                    ReleaseYear = Convert.ToInt32(row["ReleaseYear"]),
+                    Genre = row["Genre"].ToString(),
+                    CreatedDate = Convert.ToDateTime(row["CreatedDate"]),
+                    UpdatedDate = row["UpdatedDate"] == DBNull.Value
+                                  ? (DateTime?)null
+                                  : Convert.ToDateTime(row["UpdatedDate"])
+                });
+            }
+
+            return list;
+        }
+
+        private void ValidateModel(SeriesModel model)
+        {
+            if (string.IsNullOrWhiteSpace(model.Title))
+                throw new Exception("Title is required");
+
+            if (model.ReleaseYear <= 0)
+                throw new Exception("Invalid Release Year");
+        }
+    }
+}
